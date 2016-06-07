38c00f2b75f8e3084c2245c9ca7fb8914c172632
donicimarius <donicimariusflorian@gmail.com>
2016-06-07 12:39:45 +0300

    Design Admin page + Facebook post pentru fiecare articol creat...Vedem maine cum facem nu lasam asa postarea pe facebook.

--- impl/views/user/userProfile.php
+++ impl/views/user/userProfile.php
@@ -1,41 +1,52 @@
 <?php
-	require_once 'views/header.php'; 
+require_once 'views/header.php';
 ?>
 
 <main class="col-10 col-m-10 mainPage">
-<div class="section">
-	
-	
-		<h1>User Profile</h1>
-		<h2>Add hobby to preferences</h2>
-		<form class="" action="<?php echo URL.'hobbies/addPreference';?>" method="post" enctype = "multipart/form-data">
-			<div>
-				<label class="col-2">Select hobby:</label>
-				<?php
-				echo '<select id="hobby" name="hobby">';
-					echo '<option value=""></option>';
-					foreach($articles as $hobby)
-						echo '<option value="'.$hobby->getId_hobby().'">'.$hobby->getTitle().'</option>';
-				echo '</select>';
-				?>
-				<input class="" type="submit" name="submit" value="Add to preferences">
-			</div>
-			
-		</form>
-		
-		<h2>Your Hobby list:</h2>
-		<ul>
-			<?php
-			foreach($hobbies as $hobby) {
-				echo '<li>'.$hobby->getTitle().' <a href="'.URL.'hobbies/deletePreference/'.$hobby->getId_hobby().'">Delete</a></li>';
-			}
-			?>
-		</ul>
-</div>
+    <div class="section">
+
+
+        <h1>User Profile</h1>
+        <h2>Add hobby to preferences</h2>
+        <button class="toggleButton">Add a new hobby</button>
+        <div class="col-3-offset col-7 col-m-7 toggleDiv">
+            <form class="articleForm" action="<?php echo URL . 'hobbies/addPreference'; ?>" method="post"
+                  enctype="multipart/form-data">
+                <div>
+                    <label class="col-2">Select hobby:</label>
+                    <?php
+                    echo '<select class="col-2" id="hobby" name="hobby">';
+                    echo '<option value=""></option>';
+                    foreach ($articles as $hobby)
+                        echo '<option value="' . $hobby->getId_hobby() . '">' . $hobby->getTitle() . '</option>';
+                    echo '</select>';
+                    ?>
+                    <input class="col-6 col-m-6" type="text" name="motiv" placeholder="Motivul pentru care iti place acest hobby">
+                    <div class="col-10"></div>
+                    <label class="col-5 col-m-2">Share to facebook</label>
+                    <input class="col-2 col-m-1 checkBox" type="checkbox" name="shareToFacebok" id="shareToFacebook">
+                    <input class="col-3 col-m-7 formButton" type="submit" name="submit" value="Add to preferences">
+                </div>
+
+            </form>
+        </div>
+
+        <button class="toggleButton">Add a new hobby</button>
+        <div class="col-3-offset col-7 col-m-7 toggleDiv">
+            <h2>Your Hobby list:</h2>
+            <ul>
+                <?php
+                foreach ($hobbies as $hobby) {
+                    echo '<li>' . $hobby->getTitle() . ' <a href="' . URL . 'hobbies/deletePreference/' . $hobby->getId_hobby() . '">Delete</a></li>';
+                }
+                ?>
+            </ul>
+        </div>
+    </div>
 </main>
 
-<?php 
-	require_once 'views/footer.php';
+<?php
+require_once 'views/footer.php';
 ?>
 
 
